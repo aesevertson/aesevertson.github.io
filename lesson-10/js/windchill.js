5d8a3bb1ad05ec7bb378a83a3c5f582cf00f7594
aesevertson <ryd15001@byui.edu>
2019-11-23 21:45:00 -0700

    html change

--- lesson-10/js/windchill.js
+++ lesson-10/js/windchill.js
@@ -4,7 +4,7 @@ var speed = parseInt(document.getElementById("windspeed").innerText);
 
 if(temp <= 50 && speed >= 3){
     var formula = 35.74 + (0.6215*temp) - (35.75*(speed**0.16)) + (0.4275*temp*(speed**0.16));
-document.getElementById("chill").innerText = formula.toFixed(2);
+document.getElementById("chill").innerText = formula.toFixed(2) + '°F';
 
 } else {
    document.getElementById("chill").innerText = "N/A" 
