947206b78317f102c4d7f8beb70eb88f84f3bf0a
aesevertson <ryd15001@byui.edu>
2019-11-28 01:12:23 -0700

    more final site changes

--- lesson-11/js/sodaspringsinformation.js
+++ lesson-11/js/sodaspringsinformation.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=ac4a7c570cce008087083e285058e1e6";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=ac4a7c570cce008087083e285058e1e6";
 
 fetch(apiURL)
     .then((response) => response.json())
@@ -32,7 +32,7 @@ fetch(apiURL)
 
     });
 
-const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=ac4a7c570cce008087083e285058e1e6";
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&APPID=ac4a7c570cce008087083e285058e1e6";
 
 fetch(apiURL2)
     .then((response) => response.json())
@@ -64,3 +64,22 @@ fetch(apiURL2)
     function KtoF(kelvin){
         return (((kelvin - 273.15) * 9) / 5) + 32;
     }
+
+    fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const towns = jsonObject['towns'];
+    var town = towns.find(town => town.name === "Soda Springs");
+    var list = document.createElement('ul');
+    for (var event of town.events) {
+    let item = document.createElement('li');
+    item.textContent = event;
+    list.appendChild(item);
+    }
+
+    document.querySelector('div.townevents').appendChild(list);
+
+  });
