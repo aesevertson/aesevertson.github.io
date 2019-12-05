e6d59ef9d8204f93ff5bc75e586316b1c94bc52a
aesevertson <ryd15001@byui.edu>
2019-12-05 12:06:44 -0700

    temple site beginning, css preprocessor activity

---
+++ term-project/js/main.js
@@ -0,0 +1,41 @@
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+//code for current date
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+// document.getElementById("currentdate").textContent = fulldate;
+
+
+//code for banner ad
+// if (d.getDay() === 3) {
+//     document.getElementById("bannerad").hidden = false;
+// }
+
